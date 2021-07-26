c7f7182b2ae0ecd56d574748ce0973304b460ce7
kkonnov <kkonnov@phoenixit.ru>
2021-07-26 15:50:39 +0300

    token fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,22 @@
-
 import bot.RollBot;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class App {
 
     public static void main(String[] args) throws LoginException {
 
         try {
-            RollBot rollBot = new RollBot();
-        } catch (LoginException e) {
+            InputStream in = App.class.getResourceAsStream("tokens/token.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String token = reader.readLine();
+            reader.close();
+            RollBot rollBot = new RollBot(token);
+        } catch (LoginException | IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/bot/RollBot.java
+++ src/main/java/bot/RollBot.java
@@ -9,9 +9,8 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import javax.security.auth.login.LoginException;
 
 public class RollBot {
-
-    public RollBot() throws LoginException {
-        JDABuilder builder = JDABuilder.createDefault("ODY4OTg2MjExNjk0NDI4MjAx.YP3oeQ.oac9OkvjiFEilQhV1CwtlA5zm_4");
+    public RollBot(String token) throws LoginException {
+        JDABuilder builder = JDABuilder.createDefault(token);
         builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
         builder.setBulkDeleteSplittingEnabled(false);
         builder.setActivity(Activity.playing("КАЗИНО"));
@@ -19,4 +18,6 @@ public class RollBot {
         builder.addEventListeners(new HelpCommand());
         builder.build();
     }
+
+
 }
